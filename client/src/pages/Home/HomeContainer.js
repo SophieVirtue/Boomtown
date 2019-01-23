98901fda8adccde325749a227af0258b97ff3420
Sophie Virtue <sophie.virtue@gmail.com>
2019-01-23 14:21:06 -0800

    Add route's to Layout.js

--- client/src/pages/Home/HomeContainer.js
+++ client/src/pages/Home/HomeContainer.js
@@ -2,9 +2,40 @@ import React, { Component } from 'react';
 import Home from './Home';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
+// import gql from "graphql-tag";
+// import { Query } from "react-apollo";
+
+// const GET_TAGS = gql`
+//   {
+//     tags {
+//       id
+//       title
+//     }
+//   }
+// `;
 
 class HomeContainer extends Component {
   render() {
+  //   return (
+  //   <Query query={GET_TAGS}>
+  //   {({ loading, error, data }) => {
+  //     if (loading) return <p>Loading...</p>;
+  //     if (error) return `<p>Error: {error}</p>`;
+
+  //     {console.log('data', data);}
+  //    return <div></div>
+  //     // return (
+  //     //   <select name="tag" onChange={onTagsSelected}>
+  //     //     {data.tags.map(tag => (
+  //     //       <option key={tag.id} value={tag.title}>
+  //     //         {tag.title}
+  //     //       </option>
+  //     //     ))}
+  //     //   </select>
+  //     // );
+  //   }}
+  // </Query>
+  //   );
     return <Home classes={this.props.classes} />;
   }
 }
