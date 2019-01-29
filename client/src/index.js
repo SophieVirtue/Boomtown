c22b95e329da3b005801347963f1fbf966c77df1
Sophie Virtue <sophie.virtue@gmail.com>
2019-01-29 10:23:12 -0800

    Made redux actions, action creator, store, etc

--- client/src/index.js
+++ client/src/index.js
@@ -4,27 +4,13 @@ import { MuiThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ApolloProvider } from 'react-apollo'
 import { BrowserRouter } from 'react-router-dom'
-// @TODO: Uncomment each module as needed in your client app
-// import { Provider as ReduxProvider } from 'react-redux'
-
-
+import { Provider as ReduxProvider } from 'react-redux'
 import registerServiceWorker from './registerServiceWorker';
 import theme from './theme';
 import client from './apollo'
 import Layout from './routes/Layout'
+import store from './redux'
 
-
-/**
- * @TODO: Initialize Redux Store
- *
- * Uncomment the following line when your Redux store is configured
- *
- * import store from './redux'
- *
- * Below in your <App />, wrap a <ReduxProvider /> component around all
- * of the app's children, and pass it the imported `store` as the `store`
- * prop's value.
- */
 
 /**
  * @TODO: Add the Viewer Context
@@ -40,6 +26,7 @@ import './index.css';
 
 const App = () => {
   return (
+    <ReduxProvider store={store}>
     <MuiThemeProvider theme={theme}>
       <CssBaseline />
       <ApolloProvider client={client}>
@@ -48,6 +35,7 @@ const App = () => {
       </BrowserRouter>
       </ApolloProvider>
     </MuiThemeProvider>
+    </ReduxProvider>
   );
 };
 
